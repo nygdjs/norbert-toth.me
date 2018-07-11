b33408506c1b38cded077b11234b55450a1f3f32
Norbert Tóth <norbert@quince.hu>
2018-07-11 16:23:23 +0200

    Modify build css script

--- scripts/buildCSS.js
+++ scripts/buildCSS.js
@@ -1,10 +1,25 @@
-const purify = require("purify-css")
+const purify = require('purify-css');
+const sass = require('node-sass');
 
-let content = "../index.html";
-let css = "../styles/styles.css";
-let options = {
-    output: "../styles/styles.min.css",
-    min: true
-};
+function concat() {
+    sass.render(
+        {
+            file: '../styles/styles.css',
+            data: '../styles/styles_.css',
+        },
+        function(err, result) {
+            if (err) return console.log(err);
+            if (result) {
+                let content = '../index.html';
+                let css = '../styles/styles_.css';
+                let options = {
+                    output: '../styles/styles.min.css',
+                    min: true,
+                };
+                purify(content, css, options);
+            }
+        },
+    );
+}
 
-purify(content, css, options);
+concat();
